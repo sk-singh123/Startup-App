cc6fa95309b8b2969c96b179eaad76770f30a36e
Shubham Kumar Singh <shubhamsingh123kolkata@gmail.com>
2025-03-09 23:27:15 +0530

    2nd commit or screenshot

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ git init
 git clone URL_link_of_code
 #to check status of file when updated ,what updated, etc
 git status
+#to add code on git for continous updates
+git add file_name_with_extension
+#to commit or save or take screenshot of that code
+git commit -m "message_why_you commit_or_take_screenshot"
